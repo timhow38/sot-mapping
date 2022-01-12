f4c926209fb133828c63b32cfb44978b578a31ff
Riley Hunter <rileyh@imagimationschool.com>
2022-01-12 22:53:58 +1300

    Model class tested against other thingy

--- tools/test_matching.py
+++ tools/test_matching.py
@@ -3,6 +3,7 @@ from feature_extractor import *
 from image_tools import *
 from display_grid import create_display_grid
 from pathlib import Path
+from island_predictor import IslandPredictor
 import os
 import cv2
 import numpy as np
@@ -47,10 +48,28 @@ def test_one_against_all(name):
     display = create_display_grid(display_rows, display_size)
     cv2.imshow(f'Outcome', display)
 
+def test_class_against_manual():
+    display_size = 100
+    path = Path(os.path.dirname(os.path.abspath(__file__)))
+    path = path / '..' / 'testing-and-validation' / 'test-treasure-maps'
+    image_paths = path.glob('**/*.jpg')
+    display_rows = []
+    model = IslandPredictor()
+    model.precache_features()
+    cache = {}
+    for i, path in enumerate(image_paths):
+        im = cv2.imread(str(path))
+        scores = match_image(im, cache)
+        match_name = min(scores, key=lambda key: scores.get(key)[0])
+        model_match_name = model.predict(im)
+        print(f'Manual said: {match_name}, Model said: {model_match_name}')
+
 if __name__ == '__main__':
     #test_one_against_all('CrooksHollow')
-    test_all_against_all()
+    test_class_against_manual()
+    #test_all_against_all()
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    
 
 
